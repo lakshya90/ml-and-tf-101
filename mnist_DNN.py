67262254136b698e041f62e1fe4318c5fa1e8cfd
Lakshya Sivaramakrishnan <lakshya.sivaramakrishnan@iiitb.org>
2017-12-14 20:29:42 +0530

    Updated mnist

--- mnist_DNN.py
+++ mnist_DNN.py
@@ -11,12 +11,11 @@ test_labels = np.asarray(mnist.test.labels, dtype=np.int32)
 
 
 feature_columns = learn.infer_real_valued_columns_from_input(data)
-classifier = learn.LinearClassifier(feature_columns=feature_columns, n_classes=10)
+classifier = learn.DNNClassifier(feature_columns=feature_columns, n_classes=10,hidden_units=[1024,512,256])
 classifier.fit(data, labels, batch_size=100, steps=1000)
 
-classifier.evaluate(test_data, test_labels)						
+classifier.evaluate(test_data, test_labels)                                      
 accuracy_score = classifier.evaluate(test_data, test_labels)["accuracy"]
 print('Accuracy: {0:f}'.format(accuracy_score))
-
 
 
